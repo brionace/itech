bba6337fd29b34a59046b735b423f57c743d1fbb
brionace <brian@newenglish.co.uk>
2018-08-31 00:29:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,26 +6,33 @@ $(document).ready(function(){
 		
 		if( id == 'AC' ){
 			
-			$('.answer').text('').hide();
-			$('.calculus').text('0').removeClass('calculated');
-			store.length = 0;
+			clearScreen();
 			
 		}else if( id == 'SAVE' ){
 			
-		}else if( id == 'EQUALS' ){
-
-			$('.calculus').html(store).addClass('calculated');
-			$('.answer').show();
-			
 		}else{
+			
+			//Clear screen if already done calculations
+			if($('.answer').text() != '0'){
+				//clearScreen();
+			}			
 			
 			if( id != null && $(this).attr('data-symbol') != null ){
 				store.push('<span>'+$(this).attr('data-symbol')+'</span>');
 			}else{
 				store.push( $(this).text() );				
-			}
+			}			
 			
 			$('.calculus').html(store);
+			
+			if( id == 'EQUALS' ){
+
+				$('.calculus').html(store).addClass('calculated');
+				$('.answer').show();
+				doCalculations()
+
+			}			
+			
 		}
 		
 		
@@ -34,7 +41,23 @@ $(document).ready(function(){
 	console.log(store);
 	
 	function doCalculations(){
+		var newStore = [];
 		
+		
+		//Strip array of strings
+		$.each(store, function(index, value) {
+		  newStore.push($('<span/>').html(value).text());
+		});
+		
+		var answer = newStore.slice(0,-1).join("");
+		console.log(eval(answer));
+		$('.answer').text( eval(answer) )
+	}
+	
+	function clearScreen(){
+		$('.answer').hide();
+		$('.calculus').text('0').removeClass('calculated');
+		store.length = 0;		
 	}
 	
 });
